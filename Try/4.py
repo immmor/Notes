4173291d5ac545bdc6664f47489ae2a82d54bdc0
Win 10 <512543670@qq.com>
2023-08-22 08:01:06 +0800

    20230822 daily update 1

--- Try/4.py
+++ Try/4.py
@@ -1,2 +1,19 @@
-from Web.Notes.tools import claude_ai
-print(claude_ai('2+1等于几'))
+import arxiv
+
+# search = arxiv.Search(
+#     query = "ai",
+#     max_results = 10,
+#     sort_by = arxiv.SortCriterion.SubmittedDate
+# )
+
+# for result in search.results():
+#     print(result.title)
+
+
+paper = next(arxiv.Search(id_list=["1605.08386v1"]).results())
+# Download the PDF to the PWD with a default filename.
+paper.download_pdf()
+# Download the PDF to the PWD with a custom filename.
+paper.download_pdf(filename="downloaded-paper.pdf")
+# Download the PDF to a specified directory with a custom filename.
+paper.download_pdf(dirpath=".", filename="downloaded-paper.pdf")
