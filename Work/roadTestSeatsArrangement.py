7daf9f4bef7219cf9590a0bb2d84f186a4874c33
Win 10 <512543670@qq.com>
2023-10-26 08:41:02 +0800

    2023102601

---
+++ Work/roadTestSeatsArrangement.py
@@ -0,0 +1,13 @@
+"""
+路试座位安排 Road Test Seats Arrangement
+functions = ['B1', 'BT', 'Carplay', 'Audio', 'Navigation', 'Local Dips', 'BMW APP']
+internal = ['Xu Miao', 'Jerry', 'Tony', 'Boss Xie', 'Huanran', 'Zhimei', 'Marin']
+external = ['Dian Wang', 'Runqi Liu', 'Gonegjie Meng', 'Zhao Gao', 'Huanran', '']
+testers = {
+    'Dian Wang': ['BT', 'Carplay', 'Audio'], 
+    'Xu Miao': ['BT', 'Carplay', 'Audio'], 
+    'Marin': [],
+    'Gongjie': [],
+    'Tony': [],
+}
+"""
