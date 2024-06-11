011588e1c18434c180b3233b031b4957234f1321
kkkbbb <10036084+kkkbbb@user.noreply.gitee.com>
2024-06-11 11:24:47 +0800

    2024061101

--- AI/kimi.py
+++ AI/kimi.py
@@ -5,11 +5,34 @@ client = OpenAI(
     base_url="https://api.moonshot.cn/v1",
 )
 
+generateEssayPrompt = """
+        完全换一个title和content，并且计算每一个paragraph的字数输出为wordCount的值，按照这个json格式再生成一篇新的不少于300字的三段作文(title不能是换一种说法，需要完全换一个主题。不要说除了json格式以外的内容):
+        {
+            "whoCreated": "claudeAI",
+            "title": "Balancing Study and Extracurricular Activities",
+            "content": [
+                {
+                    "paragraph": "For university students, balancing academics and extracurricular activities can be challenging. While focusing on studies is important, participating in hobbies and social activities also provides benefits.",
+                    "wordCount": 86
+                },
+                {
+                    "paragraph": "Extracurriculars allow students to take a break from intense study routines. Joining sports teams, clubs and community service promotes physical health, social connections and teamwork skills. Leadership roles in organizations also build self-confidence. However, taking on too many extracurriculars can distract from academics.",
+                    "wordCount": 117
+                },
+                {
+                    "paragraph": "Therefore, students should carefully choose 1-2 extracurriculars aligned with personal interests and schedule them responsibly around study time. Focus should remain on maintaining strong grades, while allotting some time for hobbies and relationships. With proper balance, the university experience will be fulfilling both inside and outside the classroom.",
+                    "wordCount": 117
+                }
+            ],
+            "totalWordCount": 259
+        }
+    """
+
 completion = client.chat.completions.create(
     model="moonshot-v1-8k",
     messages=[
         # {"role": "system", "content": "你会为用户提供安全，有帮助，准确的回答。"},
-        {"role": "user", "content": "这个世界会变好吗"}
+        {"role": "user", "content": generateEssayPrompt}
     ],
     temperature=0.3,
 )
--- AI/zhipu.py
+++ AI/zhipu.py
@@ -1,14 +1,15 @@
 from zhipuai import ZhipuAI
+from prompt import *
 client = ZhipuAI(api_key="03a681bf3e3d7d5484e626069294d446.jKmJczxJzRT70nsF") # 填写您自己的APIKey
 response = client.chat.completions.create(
     model="glm-3-turbo",  # 填写需要调用的模型名称
     messages=[
-        {"role": "user", "content": "作为一名营销专家，请为智谱开放平台创作一个吸引人的slogan"},
+        {"role": "user", "content": generateEssayPrompt},
         {"role": "assistant", "content": "当然，为了创作一个吸引人的slogan，请告诉我一些关于您产品的信息"},
         {"role": "user", "content": "智谱AI开放平台"},
         {"role": "assistant", "content": "智启未来，谱绘无限一智谱AI，让创新触手可及!"},
         {"role": "user", "content": "创造一个更精准、吸引人的slogan"}
     ],
 )
-print(response.choices[4].message)
+print(response.choices[0].message)
     
