7c423084a71f24d2afbf4ca90c15e48a669bdbd2
kkkbbb <10036084+kkkbbb@user.noreply.gitee.com>
2024-07-09 07:04:00 +0800

    2024070901

--- Temp/main.py
+++ Temp/main.py
@@ -1,5 +1,22 @@
-# main.py
-from tasks import add
- 
-result = add.delay(4, 8)
-print(result.get())  # 输出: 8
+import meilisearch
+
+client = meilisearch.Client("http://127.0.0.1:7700", "f9JLJj870tV1fHRRWgscz8OOoe6qAkikaaSiiLvbfoI")
+
+# An index is where the documents are stored.
+index = client.index("movies")
+
+documents = [
+    {"id": 1, "title": "Carol", "genres": ["Romance", "Drama"]},
+    {"id": 2, "title": "Wonder Woman", "genres": ["Action", "Adventure"]},
+    {"id": 3, "title": "Life of Pi", "genres": ["Adventure", "Drama"]},
+    {
+        "id": 4,
+        "title": "Mad Max: Fury Road",
+        "genres": ["Adventure", "Science Fiction"],
+    },
+    {"id": 5, "title": "Moana", "genres": ["Fantasy", "Action"]},
+    {"id": 6, "title": "Philadelphia", "genres": ["Drama"]},
+]
+
+# If the index 'movies' does not exist, Meilisearch creates it when you first add the documents.
+index.add_documents(documents)  # => { "uid": 0 }
